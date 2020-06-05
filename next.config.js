369204f7a0269a37f3560dd4096b5d87fdd8ac9e
rifandani <ipandani2505@gmail.com>
2020-06-05 21:16:03 +0800

    change config.js to prod URL

--- next.config.js
+++ next.config.js
@@ -7,6 +7,6 @@ module.exports = withPWA({
   env: {
     MONGO_URI:
       'mongodb://rifandani:rifandani@cluster0-shard-00-00-rqaap.mongodb.net:27017/poem?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primaryPreferred&appname=MongoDB%20Compass&ssl=true',
-    ROOT_URL: 'production', // http://localhost:3000 or production
+    ROOT_URL: 'https://show-me-the-poem.now.sh/', // http://localhost:3000 or production URL
   },
 });
